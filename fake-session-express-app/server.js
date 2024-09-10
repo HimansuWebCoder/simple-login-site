85f40e45208402e47be6e5e8e6d975a7ea16450b
himansu naik <naikhimansu546@gmail.com>
2024-09-10 21:24:58 +0530

    updated backend

--- fake-session-express-app/server.js
+++ fake-session-express-app/server.js
@@ -70,7 +70,7 @@ app.post("/login", async (req, res) => {
         const userProfile = profile.find((profile) => profile.email === email);
 
         // Prepare response with both profile and content data
-        res.json({
+        return res.json({
             message: "Login successful",
             profile: userProfile,
             content: user.content,
@@ -91,7 +91,7 @@ app.get("/content", (req, res) => {
             );
             if (userProfile) {
                 res.json({ profile: userProfile }); // Send profile data
-                res.sendFile(path.join(__dirname, "./view.html"));
+                // res.sendFile(path.join(__dirname, "./view.html"));
             } else {
                 res.status(404).send("Profile not found");
             }
@@ -103,50 +103,6 @@ app.get("/content", (req, res) => {
     }
 });
 
-// app.post("/login", async (req, res) => {
-//     const { email, password } = req.body;
-
-//     const user = users.find((user) => user.email === email);
-//     if (user && (await bcrypt.compare(password, user.passwordHash))) {
-//         req.session.userId = user.id; // Create session
-//         console.log("Session created for user ID:", user.id); // Debugging line
-//         res.send("Login successful");
-//     } else {
-//         res.status(401).send("Invalid credentials");
-//     }
-// });
-
-// User-specific content
-// app.get("/content", (req, res) => {
-//     const user = users.find((user) => user.id === req.session.userId);
-//     if (user) {
-//         res.send(user.content); // Send content specific to logged-in user
-//     } else {
-//         res.status(401).send("Please log in to view your content");
-//     }
-// });
-
-// app.get("/content", (req, res) => {
-//     const userId = req.session.userId; // Get the userId from the session
-//     if (userId) {
-//         const user = users.find((user) => user.id === userId);
-//         if (user) {
-//             res.json({ content: user.content }); // Send content specific to the logged-in user
-//         } else {
-//             res.status(404).send("User not found");
-//         }
-//     } else {
-//         res.status(401).send("Please log in to view your content");
-//     }
-// });
-
-app.get("/test-session", (req, res) => {
-    if (req.session.userId) {
-        res.send(`Session active. User ID: ${req.session.userId}`);
-    } else {
-        res.send("No session active");
-    }
-});
 
 app.listen(3000, () => {
     console.log("Server running on port 3000");
